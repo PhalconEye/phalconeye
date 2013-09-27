d99bc9a257e14fbd84ef2fdae214996339408947
lantian <lantian.ivan@gmail.com>
2013-09-27 11:20:46 +0300

    Fixed config saving (path const) in windows.

--- app/engine/Config.php
+++ app/engine/Config.php
@@ -25,22 +25,27 @@ class Config
     public static function save($config = null)
     {
         $configText = var_export($config->toArray(), true);
+
+        // Fix pathes. This related to windows directory separator.
+        $configText = str_replace('\\\\', DS, $configText);
+
         $configText = str_replace("'" . PUBLIC_PATH, "PUBLIC_PATH . '", $configText);
         $configText = str_replace("'" . ROOT_PATH, "ROOT_PATH . '", $configText);
         $configText = '<?php
 
-/**
-* PhalconEye
-*
-* LICENSE
-*
-* This source file is subject to the new BSD license that is bundled
-* with this package in the file LICENSE.txt.
-*
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to phalconeye@gmail.com so we can send you a copy immediately.
-*
+/*
+  +------------------------------------------------------------------------+
+  | PhalconEye CMS                                                         |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2013 PhalconEye Team (http://phalconeye.com/)            |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconeye.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
 */
 
 /**
