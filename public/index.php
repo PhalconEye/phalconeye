9de18c8eb24c232eba6655d4800475460a1be87e
lantian <lantian.ivan@gmail.com>
2013-11-18 23:54:43 -0500

    Changing phalcon required version to 1.2.4

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@
 */
 error_reporting(E_ALL);
 define('PE_VERSION', '0.4.0');
-define('PHALCON_VERSION_REQUIRED', '1.2.0');
+define('PHALCON_VERSION_REQUIRED', '1.2.4');
 define('PHP_VERSION_REQUIRED', '5.4.0');
 define('DS', DIRECTORY_SEPARATOR);
 if (!defined('ROOT_PATH')) {
