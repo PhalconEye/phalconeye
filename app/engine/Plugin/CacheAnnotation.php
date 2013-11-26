00a0594aa87e8157581e655a01175c7673a743fb
lantian <lantian.ivan@gmail.com>
2013-11-26 19:05:28 +0200

    Plugin namespace changed according to standards.

--- app/engine/Plugin/CacheAnnotation.php
+++ app/engine/Plugin/CacheAnnotation.php
@@ -1,57 +1,77 @@
 <?php
-
-/**
- * PhalconEye
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- *
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to phalconeye@gmail.com so we can send you a copy immediately.
- *
- */
+/*
+  +------------------------------------------------------------------------+
+  | PhalconEye CMS                                                         |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2013 PhalconEye Team (http://phalconeye.com/)            |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconeye.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Author: Ivan Vorontsov <ivan.vorontsov@phalconeye.com>                 |
+  +------------------------------------------------------------------------+
+*/
 
 namespace Engine\Plugin;
 
-class CacheAnnotation extends \Phalcon\Mvc\User\Plugin
-{
+use Phalcon\Mvc\User\Plugin as PhalconPlugin;
+use Phalcon\Dispatcher;
+use Phalcon\Events\Event;
 
+/**
+ * Cache plugin.
+ *
+ * @category  PhalconEye
+ * @package   Engine\Plugin
+ * @author    Ivan Vorontsov <ivan.vorontsov@phalconeye.com>
+ * @copyright 2013 PhalconEye Team
+ * @license   New BSD License
+ * @link      http://phalconeye.com/
+ */
+class CacheAnnotation extends PhalconPlugin
+{
     /**
-     * This event is executed before every route is executed in the dispatcher
+     * This event is executed before every route is executed in the dispatcher.
      *
+     * @param Event      $event      Event object.
+     * @param Dispatcher $dispatcher Dispatcher object.
+     *
+     * @return bool
      */
     public function beforeExecuteRoute($event, $dispatcher)
     {
-        //Parse the annotations in the method currently executed
+        // Parse the annotations in the method currently executed.
         $annotations = $this->annotations->getMethod(
             $dispatcher->getActiveController(),
             $dispatcher->getActiveMethod()
         );
 
-        //Check if the method has an annotation 'Cache'
+        // Check if the method has an annotation 'Cache'.
         if ($annotations->has('Cache')) {
 
-            //The method has the annotation 'Cache'
+            // The method has the annotation 'Cache'.
             /** @var \Phalcon\Annotations\Annotation $annotation */
             $annotation = $annotations->get('Cache');
 
-            //Get the lifetime
+            // Get the lifetime.
             $lifetime = $annotation->getNamedArgument('lifetime');
 
             $options = array('lifetime' => $lifetime);
 
-            //Check if there is a user defined cache key
+            // Check if there is a user defined cache key.
             if ($annotation->hasNamedArgument('key')) {
                 $options['key'] = $annotation->getNamedArgument('key');
             }
 
-            //Enable the cache for the current method
+            // Enable the cache for the current method.
             $this->view->cache($options);
         }
 
+        return $event->isStopped();
     }
 
 }
--- app/engine/Plugin/NotFound.php
+++ app/engine/Plugin/NotFound.php
@@ -1,43 +1,66 @@
 <?php
-
-/**
- * PhalconEye
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- *
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to phalconeye@gmail.com so we can send you a copy immediately.
- *
- */
+/*
+  +------------------------------------------------------------------------+
+  | PhalconEye CMS                                                         |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2013 PhalconEye Team (http://phalconeye.com/)            |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconeye.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Author: Ivan Vorontsov <ivan.vorontsov@phalconeye.com>                 |
+  +------------------------------------------------------------------------+
+*/
 
 namespace Engine\Plugin;
 
-class NotFound extends \Phalcon\Mvc\User\Plugin
+use Engine\Application as EngineApplication;
+use Phalcon\Dispatcher;
+use Phalcon\Events\Event;
+use Phalcon\Exception as PhalconException;
+use Phalcon\Mvc\User\Plugin as PhalconPlugin;
+
+/**
+ * Not found plugin.
+ *
+ * @category  PhalconEye
+ * @package   Engine\Plugin
+ * @author    Ivan Vorontsov <ivan.vorontsov@phalconeye.com>
+ * @copyright 2013 PhalconEye Team
+ * @license   New BSD License
+ * @link      http://phalconeye.com/
+ */
+class NotFound extends PhalconPlugin
 {
     /**
-     * @param \Phalcon\Events\Event $event
-     * @param \Phalcon\Dispatcher $dispatcher
-     * @param \Phalcon\Exception $exception
+     * Before exception is happening.
      *
-*@return bool
+     * @param Event            $event      Event object.
+     * @param Dispatcher       $dispatcher Dispatcher object.
+     * @param PhalconException $exception  Exception object.
+     *
+     * @return bool
      */
     public function beforeException($event, $dispatcher, $exception)
     {
         switch ($exception->getCode()) {
-            case \Phalcon\Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
-            case \Phalcon\Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
+            case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
+            case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
                 $dispatcher->forward(array(
-                    'module' => \Engine\Application::$defaultModule,
-                    'namespace' => ucfirst(\Engine\Application::$defaultModule) . '\Controller',
+                    'module' => EngineApplication::$defaultModule,
+                    'namespace' => ucfirst(EngineApplication::$defaultModule) . '\Controller',
                     'controller' => 'error',
                     'action' => 'show404'
                 ));
+
                 return false;
         }
+
+        return $event->isStopped();
     }
 
 }
