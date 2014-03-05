f7c564167bd2c6f2016b3ac670f422bcc62647ca
lantian <lantian.ivan@gmail.com>
2014-03-05 10:42:19 +0200

    Fixed phpcs error.

--- app/modules/Core/Controller/InstallController.php
+++ app/modules/Core/Controller/InstallController.php
@@ -107,8 +107,9 @@ class InstallController extends AbstractController
      */
     public function indexAction()
     {
-        if ($_SERVER['REQUEST_URI'] != $this->config->application->baseUrl){
-            echo "You trying to access via '{$_SERVER['REQUEST_URI']}' path... But config has '{$this->config->application->baseUrl}' as base url.";
+        if ($_SERVER['REQUEST_URI'] != $this->config->application->baseUrl) {
+            echo "You trying to access via '{$_SERVER['REQUEST_URI']}' path...
+            But config has '{$this->config->application->baseUrl}' as base url.";
             exit(1);
         }
 
