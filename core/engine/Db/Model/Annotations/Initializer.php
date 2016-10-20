454bf57eb7a0d54aee8c0cc214dcbae3a341f845
lantian <lantian.ivan@gmail.com>
2016-10-20 18:56:15 +0300

    Applied patch from https://github.com/PhalconEye/cms/pull/78. Missed files for previous commit.

--- core/engine/Db/Model/Annotations/Initializer.php
+++ core/engine/Db/Model/Annotations/Initializer.php
@@ -12,7 +12,8 @@
   | obtain it through the world-wide-web, please send an email             |
   | to license@phalconeye.com so we can send you a copy immediately.       |
   +------------------------------------------------------------------------+
-  | Author: Ivan Vorontsov <lantian.ivan@gmail.com>                 |
+  | Author: Ivan Vorontsov <lantian.ivan@gmail.com>                        |
+  | Author: Piotr Gasiorowski <p.gasiorowski@vipserv.org>                  |
   +------------------------------------------------------------------------+
 */
 
@@ -29,6 +30,7 @@ use Phalcon\Mvc\User\Plugin;
  * @category  PhalconEye
  * @package   Engine\Db\Model\Annotations
  * @author    Ivan Vorontsov <lantian.ivan@gmail.com>
+ * @author    Piotr Gasiorowski <p.gasiorowski@vipserv.org>
  * @copyright 2013-2016 PhalconEye Team
  * @license   New BSD License
  * @link      http://phalconeye.com/
@@ -58,12 +60,13 @@ class Initializer extends Plugin
              * Traverse the annotations
              */
             foreach ($annotations as $annotation) {
+                $arguments = $annotation->getArguments();
+
                 switch ($annotation->getName()) {
                     /**
                      * Initializes the model's source
                      */
                     case 'Source':
-                        $arguments = $annotation->getArguments();
                         $manager->setModelSource($model, $arguments[0]);
                         break;
 
@@ -71,24 +74,24 @@ class Initializer extends Plugin
                      * Initializes Has-Many relations
                      */
                     case 'HasMany':
-                        $arguments = $annotation->getArguments();
-                        if (isset($arguments[3])) {
-                            $manager->addHasMany($model, $arguments[0], $arguments[1], $arguments[2], $arguments[3]);
-                        } else {
-                            $manager->addHasMany($model, $arguments[0], $arguments[1], $arguments[2]);
-                        }
+                        array_unshift($arguments, $model);
+                        call_user_func_array([$manager, 'addHasMany'], $arguments);
+                        break;
+
+                    /**
+                     * Initializes Has-Many-To-Many relations
+                     */
+                    case 'HasManyToMany':
+                        array_unshift($arguments, $model);
+                        call_user_func_array([$manager, 'addHasManyToMany'], $arguments);
                         break;
 
                     /**
                      * Initializes BelongsTo relations
                      */
                     case 'BelongsTo':
-                        $arguments = $annotation->getArguments();
-                        if (isset($arguments[3])) {
-                            $manager->addBelongsTo($model, $arguments[0], $arguments[1], $arguments[2], $arguments[3]);
-                        } else {
-                            $manager->addBelongsTo($model, $arguments[0], $arguments[1], $arguments[2]);
-                        }
+                        array_unshift($arguments, $model);
+                        call_user_func_array([$manager, 'addBelongsTo'], $arguments);
                         break;
                 }
             }
