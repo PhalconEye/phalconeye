96a449aae817c9a85fb9109f8e29036239c57a70
lantian <lantian.ivan@gmail.com>
2016-10-31 23:59:57 +0300

    Lost in previous commit

--- app/widgets/Demo/metadata.php
+++ app/widgets/Demo/metadata.php
@@ -1,4 +1,6 @@
 <?php
 return [
+    'version' => PHALCONEYE_VERSION,
+    'author' => 'Piotr Gasiorowski <p.gasiorowski@vipserv.org>',
     'description' => 'Displays forms and other structures'
 ];
